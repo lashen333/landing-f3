4363eab23ceef456e65633258e5d8f51daf6ff53
lashen333 <lashen.aenigm3labs@gmail.com>
2025-10-17 14:22:26 +0530

    Version6.2-Geo update

--- src/components/dashboard/GeoLeaflet.tsx
+++ src/components/dashboard/GeoLeaflet.tsx
@@ -17,7 +17,7 @@ export default function GeoLeaflet({ points }: { points: Point[] }) {
       <MapContainer {...({ center: [20, 0], zoom: 2, minZoom: 2, worldCopyJump: true } )} style={{ height: "100%", width: "100%", background: "#0b0f1a" }}>
       <TileLayer {...({ attribution: '&copy; OpenStreetMap', url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" } )} />
       {data.map((p, i) => (
-          // @ts-ignore - react-leaflet CircleMarker radius prop may be missing from some typings
+          // @ts-expect-error - react-leaflet CircleMarker radius prop may be missing from some typings
           <CircleMarker key={i} center={[p.lat, p.lon]} radius={radius(p.sessions)} pathOptions={{ color: "#fff", weight: 1, fillColor: "#fff", fillOpacity: 0.75 }}>
             <Tooltip {...{ direction: "top", offset: [0, -6], opacity: 0.9 }}>
               {(p.city ? p.city + ", " : "") + (p.countryCode ?? "")}<br />
